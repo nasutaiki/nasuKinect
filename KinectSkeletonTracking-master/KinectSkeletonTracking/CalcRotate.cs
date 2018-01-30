803a0422890ccdde30b821c27f4dd609d4d42ea5
nasutaiki <33119501+nasutaiki@users.noreply.github.com>
2018-01-30 15:04:19 +0900

    Add files via upload

---
+++ KinectSkeletonTracking-master/KinectSkeletonTracking/CalcRotate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.Kinect;
+
+/*
+ https://lightbuzz.com/kinect-joint-rotations/ から持ってきたソース。
+ 計算に関しては未把握。
+*/
+
+namespace KinectSkeletonTracking
+{
+    public class CalcRotate
+    {
+        /// <summary>
+        /// Rotates the specified quaternion around the X axis.
+        /// </summary>
+        /// <param name="quaternion">The orientation quaternion.</param>
+        /// <returns>The rotation in degrees.</returns>
+        public static double Pitch(Vector4 quaternion)
+        {
+            double value1 = 2.0 * (quaternion.W * quaternion.X + quaternion.Y * quaternion.Z);
+            double value2 = 1.0 - 2.0 * (quaternion.X * quaternion.X + quaternion.Y * quaternion.Y);
+
+            double roll = Math.Atan2(value1, value2);
+
+            return roll * (180.0 / Math.PI);
+        }
+
+        /// <summary>
+        /// Rotates the specified quaternion around the Y axis.
+        /// </summary>
+        /// <param name="quaternion">The orientation quaternion.</param>
+        /// <returns>The rotation in degrees.</returns>
+        public static double Yaw(Vector4 quaternion)
+        {
+            double value = 2.0 * (quaternion.W * quaternion.Y - quaternion.Z * quaternion.X);
+            value = value > 1.0 ? 1.0 : value;
+            value = value < -1.0 ? -1.0 : value;
+
+            double pitch = Math.Asin(value);
+
+            return pitch * (180.0 / Math.PI);
+        }
+
+        /// <summary>
+        /// Rotates the specified quaternion around the Z axis.
+        /// </summary>
+        /// <param name="quaternion">The orientation quaternion.</param>
+        /// <returns>The rotation in degrees.</returns>
+        public static double Roll(Vector4 quaternion)
+        {
+            double value1 = 2.0 * (quaternion.W * quaternion.Z + quaternion.X * quaternion.Y);
+            double value2 = 1.0 - 2.0 * (quaternion.Y * quaternion.Y + quaternion.Z * quaternion.Z);
+
+            double yaw = Math.Atan2(value1, value2);
+
+            return yaw * (180.0 / Math.PI);
+        }
+    }
+}
---
+++ KinectSkeletonTracking-master/KinectSkeletonTracking/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using Microsoft.Kinect;
+
+namespace KinectSkeletonTracking
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor kinect;
+
+        BodyFrameReader bodyFrameReader; //
+        Body[] bodies; // Bodyを保持する配列；Kinectは最大6人トラッキングできる
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Windowが表示されたときコールされる
+        private void Window_Loaded(object sensor, RoutedEventArgs e)
+        {
+            try
+            {
+                kinect = KinectSensor.GetDefault();
+                // TODO: Kinectが使用可能状態か調べてから次の処理に移りたい。
+                kinect.Open();
+
+                // Bodyを入れる配列を作る
+                bodies = new Body[kinect.BodyFrameSource.BodyCount];
+
+                // body用のフレームリーダーを取得する。30fps?
+                bodyFrameReader = kinect.BodyFrameSource.OpenReader();
+                //ここでイベントハンドラを追加する。Kinectが撮影したBodyのフレームデータが到着したときに通知される。
+                bodyFrameReader.FrameArrived += bodyFrameReader_FrameArrived;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+        }
+
+
+        // Windowが閉じたときにコールされる
+        private void Window_Closing(object sensor, System.ComponentModel.CancelEventArgs e)
+        {
+            if (bodyFrameReader != null)
+            {
+                bodyFrameReader.Dispose();
+                bodyFrameReader = null;
+            }
+
+            if (kinect != null)
+            {
+                kinect.Close();
+                kinect = null;
+            }
+        }
+        
+        // イベント発生はこのメソッドに通知される？
+        void bodyFrameReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            UpdateBodyFrame(e); // ボディデータの更新をする
+            // DrawBodyFrame(); // TODO:GUIに対する描写は後に実装する
+            SendRotate();    // 角度を取得して送信する
+        }
+
+        // ボディの更新をする。イベントハンドラ（フレームが取得できた、イベントが発生した ときにコールされる）
+        private void UpdateBodyFrame(BodyFrameArrivedEventArgs e)
+        {
+            
+            // usingブロック内で宣言された変数はGCに任せずに確実に開放される
+            // フレームはKinectから送られてくるデータの最小単位。e.FrameReference.AcquireFrame()で取得する。
+            using (var bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                // nullが送られてくる可能性があるので、その場合は破棄。
+                if (bodyFrame == null)
+                {
+                    return;
+                }
+
+                // ボディデータをbodiesにコピーする
+                bodyFrame.GetAndRefreshBodyData(bodies);
+            }
+        }
+
+        private void SendRotate()
+        {
+            // 追跡しているBodyのみループする
+            foreach (var body in bodies.Where(b => b.IsTracked))
+            {
+                // Bodyから取得した全関節でループする。
+                foreach (var joint in body.Joints)
+                {
+                    // 追跡可能な状態か？
+                    if (joint.Value.TrackingState == TrackingState.Tracked)
+                    {
+                        //関節の向きを取得する（Vector4型）。関節の指定にはJoyntType(enum)を使用する。
+                        var orientation = body.JointOrientations[joint.Key].Orientation;
+
+                        //関節のそれぞれの軸に対応する角度を取得する
+                        var pitchRotate = CalcRotate.Pitch(orientation);
+                        var yowRotate = CalcRotate.Yaw(orientation);
+                        var rollRotate = CalcRotate.Roll(orientation);
+                        
+                        // TODO:↑の角度の値から必要なものをソケット通信で送信する
+                    }
+                }
+            }
+        }
+    }
+}
